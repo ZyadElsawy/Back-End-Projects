c39114a48969dfcf1de788181639098d1dfdd5b9
Zyad Elsawy <zayd202096@gmail.com>
2025-11-14 07:41:35 +0200

    Added Basic CRUD Operations

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,22 +1,48 @@
 const Task = require("../models/Task");
 
-exports.getAllTasks = (req, res) => {
-  res.send({ message: "getting all tasks" });
+exports.getAllTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.status(200).json({ tasks });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 exports.createTask = async (req, res) => {
-  const task = await Task.create(req.body);
-  res.status(201).json({ task });
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
-exports.getSingleTask = (req, res) => {
-  res.send({ message: "Returning Single Task", id: req.params.id });
+exports.getSingleTask = async (req, res) => {
+  try {
+    const task = await Task.findById(req.params.id, "name");
+    res.status(201).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
-exports.updateTask = (req, res) => {
-  res.send({ message: "updating task", id: req.params.id });
+exports.updateTask = async (req, res) => {
+  try {
+    const task = await Task.findByIdAndUpdate(req.params.id, {
+      name: req.body.name,
+    });
+    res.status(200).json({ message: "Task Updated" });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
-exports.deleteTask = (req, res) => {
-  res.send({ message: "Deleting Task", id: req.params.id });
+exports.deleteTask = async (req, res) => {
+  try {
+    const task = await Task.findByIdAndDelete(req.params.id);
+    res.status(200).json({ message: "Task Deleted" });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
--- models/Task.js
+++ models/Task.js
@@ -1,8 +1,16 @@
 const mongoose = require("mongoose");
 
 const taskSchema = new mongoose.Schema({
-  name: String,
-  completed: Boolean,
+  name: {
+    type: String,
+    required: [true, "Name Cannot Be Empty"],
+    maxlength: [20, "name should not be more than 20 characters"],
+    trim: true,
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 module.exports = mongoose.model("Task", taskSchema);
